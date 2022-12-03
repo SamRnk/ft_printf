bd38fba8d05358d099156d3b422ba4e5bc220c0e
Sam Reerink <sreerink@f0r3s10.codam.nl>
2022-12-03 20:33:12 +0100

    Some work on the set-up

--- ft_printf.c
+++ ft_printf.c
@@ -1,3 +1,28 @@
 #include	"ft_printf.h"
 
-int	ft_printf(const char *s, ...)
+int	ft_print_format(int format, va_list args)
+
+int	ft_printf(const char *format, ...)
+{
+	size_t	i;
+	va_list	args;
+	va_start(args, format);
+
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] != '\0')
+				ft_print_format(format[i], args);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	ft_printf("%c", 'g');
+}
